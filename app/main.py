e06f68f2dc5e858aa77c3fe0174f792354e216ec
AleDella18 <aledella18@gmail.com>
2026-07-10 12:52:30 +0200

    feat: restored old cors setup

--- app/main.py
+++ app/main.py
@@ -35,9 +35,8 @@ app.mount("/imgs", StaticFiles(directory=str(IMGS_DIR)), name="imgs")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        # "https://derivate-calculator-frontend.vercel.app",
-        # "http://localhost:3000"
-        "*"
+        "https://derivate-calculator-frontend.vercel.app",
+        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
